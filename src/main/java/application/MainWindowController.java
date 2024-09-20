082a94360bb8e621e1e711918e0bbdb47b483a97
Daniil Italev <italyevdm03@gmail.com>
2024-09-20 17:14:44 +0300

    develop interface

--- src/main/java/application/MainWindowController.java
+++ src/main/java/application/MainWindowController.java
@@ -28,11 +28,15 @@ public class MainWindowController {
     private Button btnLine;
 
     @FXML
+    private Button btnRectangle;
+
+    @FXML
     void initialize() {
         assert btn2D != null : "fx:id=\"btn2D\" was not injected: check your FXML file 'mainWindow.fxml'.";
         assert btn3D != null : "fx:id=\"btn3D\" was not injected: check your FXML file 'mainWindow.fxml'.";
         assert btnDot != null : "fx:id=\"btnDot\" was not injected: check your FXML file 'mainWindow.fxml'.";
         assert btnLine != null : "fx:id=\"btnLine\" was not injected: check your FXML file 'mainWindow.fxml'.";
+        assert btnRectangle != null : "fx:id=\"btnRectangle\" was not injected: check your FXML file 'mainWindow.fxml'.";
 
     }
 
