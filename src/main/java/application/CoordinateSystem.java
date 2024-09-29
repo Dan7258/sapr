c7bf6945bfc7d7235b91709a5d6f0423b78a11e5
Daniil Italev <italyevdm03@gmail.com>
2024-09-29 14:45:00 +0300

    develop доделал панормаирование

--- src/main/java/application/CoordinateSystem.java
+++ src/main/java/application/CoordinateSystem.java
@@ -11,32 +11,39 @@ public class CoordinateSystem {
 
     private Line xAxis;
     private Line yAxis;
-    private SplitPane splitArea;
+    private double[] xAxisPosition = new double[4];
+    private double[] yAxisPosition = new double[4];
     private AnchorPane workingArea;
     private Label mouseCoordinates;
 
     private int oldMouseCoordX;
     private int oldMouseCoordY;
-    private int oldAreaPositionX;
-    private int oldAreaPositionY;
     
     public CoordinateSystem(SplitPane splitArea, AnchorPane workingArea, Label mouseCoordinates) {
         this.workingArea = workingArea;
         this.mouseCoordinates = mouseCoordinates;
-        this.splitArea = splitArea;
         createAxes();
         showMouseCoordinates();
         enablePanning();
+        
     } 
 
 
     private void createAxes() {
-        double width = workingArea.getPrefWidth();
-        double height = workingArea.getPrefHeight();
+        xAxisPosition[0] = 0;
+        xAxisPosition[1] = 850;
+        xAxisPosition[2] = workingArea.getPrefWidth();;
+        xAxisPosition[3] = 850;
 
-        xAxis = new Line(0, 850, width, 850);
+        yAxisPosition[0] = 50;
+        yAxisPosition[1] = 0;
+        yAxisPosition[2] = 50;
+        yAxisPosition[3] = workingArea.getPrefHeight();
+
+
+        xAxis = new Line(xAxisPosition[0], xAxisPosition[1], xAxisPosition[2], xAxisPosition[3]);
         xAxis.setStyle("-fx-stroke: black;");
-        yAxis = new Line(50, 0, 50, height);
+        yAxis = new Line(yAxisPosition[0], yAxisPosition[1], yAxisPosition[2], yAxisPosition[3]);
         yAxis.setStyle("-fx-stroke: black;");
 
         workingArea.getChildren().addAll(xAxis, yAxis);
@@ -51,31 +58,41 @@ public class CoordinateSystem {
     } 
 
     private void enablePanning() {
-
         workingArea.setOnMousePressed(event -> {
             if(event.isPrimaryButtonDown() && event.isSecondaryButtonDown()) {
                 oldMouseCoordX = (int)event.getSceneX();
                 oldMouseCoordY = (int)event.getSceneY();
-                oldAreaPositionX = (int)workingArea.getTranslateX();
-                oldAreaPositionY = (int)workingArea.getTranslateY();
+                
+                xAxisPosition[1] = xAxis.getStartY();
+                xAxisPosition[2] = xAxis.getEndX();
+                xAxisPosition[3] = xAxis.getEndY();
+
+                yAxisPosition[0] = yAxis.getStartX();
+                yAxisPosition[2] = yAxis.getEndX();
+                yAxisPosition[3] = yAxis.getEndY();
             }
-
-            
         });
-
+    
         workingArea.setOnMouseDragged(event -> {
             if(event.isPrimaryButtonDown() && event.isSecondaryButtonDown()) {
                 double deltaX = event.getSceneX() - oldMouseCoordX;
                 double deltaY = event.getSceneY() - oldMouseCoordY;
-                workingArea.setTranslateX(oldAreaPositionX + deltaX);
-                workingArea.setTranslateY(oldAreaPositionY + deltaY);
-                
-                
-            }
 
-            
+                xAxis.setStartY(xAxisPosition[1] + deltaY);
+                xAxis.setEndY(xAxisPosition[3] + deltaY);
+
+                yAxis.setStartX(yAxisPosition[0] + deltaX);
+                yAxis.setEndX(yAxisPosition[2] + deltaX);
+                
+            }            
         });
-
     }
 
+    public double[] getXAxesCoord() {
+        return xAxisPosition;
+    }
+
+    public double[] getYAxesCoord() {
+        return yAxisPosition;
+    }
 }
